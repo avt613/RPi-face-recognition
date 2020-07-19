8b1713b8026eb75b02fa3f72157827faa727e4a4
avt613 <avrohomtravers@gmail.com>
2020-07-19 23:28:01 +0100

    started integrating person page

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def settings_page():
     settings = db.execute("SELECT * FROM settings ORDER BY id ASC")
     return render_template("/settings.html", settings=settings)
 
-@app.route("/settings/settings-update", methods=["POST"])
+@app.route("/settings/update", methods=["POST"])
 def settings_update():
     setting_id = request.form.get("id")
     setting_name = request.form.get("name")
@@ -31,13 +31,13 @@ def settings_update():
     settings = db.execute("UPDATE settings SET name=?, value=? WHERE id=?", setting_name, setting_value, setting_id)
     return redirect("/settings")
 
-@app.route("/settings/settings-delete", methods=["POST"])
+@app.route("/settings/delete", methods=["POST"])
 def settings_delete():
     setting_id = request.form.get("id")
     settings = db.execute("DELETE FROM settings WHERE id=?", setting_id)
     return redirect("/settings")
 
-@app.route("/settings/settings-new", methods=["POST"])
+@app.route("/settings/new", methods=["POST"])
 def settings_new():
     setting_name = request.form.get("name")
     setting_value = request.form.get("value")
@@ -75,5 +75,31 @@ def person_page(person_id):
     log = db.execute("SELECT * FROM log LEFT JOIN people ON person_id = people.id WHERE person_id LIKE ? OR datetime IN (SELECT datetime FROM log WHERE person_id LIKE ?) ORDER BY datetime DESC",person_id ,person_id)
     return render_template("/person.html", person=person, log=log)
 
+#--------------------------------
+@app.route("/person/delete", methods=["POST"])
+def person_delete():
+    del0 = request.form.get("del0")
+    del1 = request.form.get("del1")
+    del2 = request.form.get("del2")
+    del3 = request.form.get("del3")
+    del4 = request.form.get("del4")
+    if del0 = del1 = del2 = del3 = del4 = TRUE:
+        id = request.form.get("id")
+        faces = db.execute("SELECT image_loc FROM faces WHERE person_id=?", id)
+        for image in faces:
+            os.remove(image)
+        db.execute("DELETE FROM faces WHERE person_id=?", id)
+        db.execute("DELETE FROM people WHERE id=?", id)
+        return redirect("/people.html/")
+    return redirect("/person.html/" + id)
+
+#--------------------------------
+@app.route("/person/update", methods=["POST"])
+def person_update():
+    id = request.form.get("id")
+    name = request.form.get("name")
+    trusted = request.form.get("trusted")
+    announce = request.form.get("announce")
+    db.execute("UPDATE people SET name=?, trusted=?, announce=? WHERE id=?",name ,trusted ,announce ,id)
 #--------------------------------
 
