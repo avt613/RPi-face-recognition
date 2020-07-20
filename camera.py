9666bd9e5900d62749464d60e8684ff027cf3efe
avt613 <avrohomtravers@gmail.com>
2020-07-20 12:31:47 +0100

    added camera script

---
+++ camera.py
@@ -0,0 +1,59 @@
+import face_recognition
+import picamera
+import numpy as np
+from config import *
+from datetime import datetime
+from PIL import Image, ImageDraw
+import uuid
+
+camera = picamera.PiCamera()
+camera.rotation = 180
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+print("Loading known face image(s)")
+res = dbget()
+known_face_ids = res[0]
+known_face_names = res[1]
+trusted = res[2]
+announce = res[3]
+known_face_encoding = res[4]
+
+while True:
+    camera.capture(output, format="rgb")
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+
+    faces = Image.fromarray(output)
+
+    # Loop over each face found in the frame to see if it's someone we know.
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        match = face_recognition.compare_faces(known_face_encoding, face_encoding, tolerance=0.5)
+        face_distances = face_recognition.face_distance(known_face_encoding, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if match[best_match_index]:
+            name = known_face_names[best_match_index]
+            person_id = known_face_ids[best_match_index]
+        else:
+            unique = uuid.uuid1()
+            name = "Unknown-" + str(unique)
+            extrawidth = 0.2*(right - left)
+            extraheight = 0.2*(bottom - top)
+            face = faces.crop(((left - extrawidth), (top - 3*extraheight), (right + extrawidth), (bottom + extraheight)))
+            image_loc = people_dir + str(unique) + ".JPG"
+            face.save(image_loc)
+            face.close()
+            person_id = db_person_add(name)
+            db_face_add(person_id, image_loc, face_encoding)
+            #---------- Reload faces
+            print("Loading known face image(s)")
+            res = dbget()
+            known_face_ids = res[0]
+            known_face_names = res[1]
+            trusted = res[2]
+            announce = res[3]
+            known_face_encoding = res[4]
+        timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+        db.execute('INSERT INTO log ("person_id", "datetime", "distance") VALUES(?, ?, ?)',person_id, timestamp, float(face_distances[best_match_index]))
+        print(name, timestamp, float(face_distances[best_match_index]))
