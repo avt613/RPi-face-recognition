5b4f820f8b3ec1b8c41268d8337aee74cced5aca
avt613 <avrohomtravers@gmail.com>
2020-07-31 14:39:30 +0100

    added live preview

--- services/install.sh
+++ services/install.sh
@@ -4,3 +4,4 @@ sudo systemctl start camera.service
 sudo cp gunicorn.service /etc/systemd/system
 sudo systemctl enable gunicorn.service
 sudo systemctl start gunicorn.service
+sudo cp live.service /etc/systemd/system
