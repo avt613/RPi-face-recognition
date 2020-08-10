a76520aa187bc140756d9b41af82ff50dddee3e2
avt613 <avrohomtravers@gmail.com>
2020-08-10 23:32:49 +0100

    Update install.sh

--- services/install.sh
+++ services/install.sh
@@ -1,7 +1,13 @@
-sudo cp camera.service /etc/systemd/system
+sudo cp camera.service /etc/systemd/system/camera.service
 sudo systemctl enable camera.service
 sudo systemctl start camera.service
-sudo cp gunicorn.service /etc/systemd/system
+
+sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
 sudo systemctl enable gunicorn.service
 sudo systemctl start gunicorn.service
-sudo cp live.service /etc/systemd/system
+
+sudo cp keypad.service /etc/systemd/system/keypad.service
+sudo systemctl enable keypad.service
+sudo systemctl start keypad.service
+
+sudo cp live.service /etc/systemd/system/live.service
