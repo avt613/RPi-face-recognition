bb0338a46c480231ffdd40525e59266052a8b775
avt613 <avrohomtravers@gmail.com>
2020-08-23 10:59:29 +0100

    added service status and optimized restart buttons

--- configs/services.py
+++ configs/services.py
@@ -1,4 +1,5 @@
 import os
+services_list = ['camera', 'gunicorn', 'keypad', 'fan', 'live']
 def restart_service(name):
     #print('sudo systemctl restart ' + name + '.service')
     return os.system('sudo systemctl restart ' + name + '.service')
@@ -6,3 +7,10 @@ def restart_service(name):
 def stop_service(name):
     #print('sudo systemctl stop ' + name + '.service')
     return os.system('sudo systemctl stop ' + name + '.service')
+    
+def service_active(name):
+    service_status = os.system('systemctl is-active --quiet ' + name)
+    if service_status == 0:
+        return True
+    else:
+        return False
