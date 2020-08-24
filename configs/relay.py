32739958f5626b754ef199cd00c6e549c0b926e5
avt613 <avrohomtravers@gmail.com>
2020-08-24 20:38:55 +0100

    Update relay.py

--- configs/relay.py
+++ configs/relay.py
@@ -9,18 +9,11 @@ GPIO.setwarnings(False)
 GPIO.setup(relay_pin_1,GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(relay_pin_2,GPIO.OUT, initial=GPIO.HIGH)
 def door_open(unlock_time):
-    GPIO.output(relay_pin_1,GPIO.HIGH)
+    GPIO.output(relay_pin_1,GPIO.HIGH) # If you are not using a latching relay then this line must be deleted.
     GPIO.output(relay_pin_2,GPIO.LOW)
     print('Door unlocked')
     sleep(unlock_time)
-    GPIO.output(relay_pin_1,GPIO.LOW)
+    GPIO.output(relay_pin_1,GPIO.LOW) # If you are not using a latching relay then this line can be deleted.
     GPIO.output(relay_pin_2,GPIO.HIGH)
     print('Door locked')
-    
-def door_status():
-    if GPIO.input(relay_pin_1) == GPIO.LOW and GPIO.input(relay_pin_2) == GPIO.HIGH:
-        return "LOCKED"
-    elif GPIO.input(relay_pin_1) == GPIO.HIGH and GPIO.input(relay_pin_2) == GPIO.LOW:
-        return "UNLOCKED"
-    else:
-        return "Something went wrong"
+
