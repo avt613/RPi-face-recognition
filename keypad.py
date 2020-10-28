81ee98cfd4265da28b3d4812e9e13b08e136814e
avt613 <avrohomtravers@gmail.com>
2020-10-28 17:40:30 +0000

    added master pin

--- keypad.py
+++ keypad.py
@@ -2,7 +2,7 @@ from configs.matrixKeypad_RPi_GPIO import keypad
 from configs.relay import *
 from configs.camera import *
 from time import sleep
-from configs.config import db
+from configs.config import db, masterpin, noneid
 from configs.telegram import diag
 from datetime import datetime
 
@@ -46,37 +46,43 @@ while True:
         code.append(key)
         diag('keypad.py: Key pressed:' + str(key))
     elif key == "*" and len(code) >= 1:
-        #code.pop()
-        code = []
+        #code.pop() #remove last digit from code
+        code = [] #reset code
         diag('keypad.py: Key pressed:' + str(key))
     elif key == "#":
         tocheck = arrtostr(code)
         print(tocheck)
-        diag('keypad.py: Code entered:' + tocheck)
-        if tocheck in saved_pins_pin and saved_pins_trusted[saved_pins_pin.index(tocheck)] == 'True':
-            person_id = saved_pins_id[saved_pins_pin.index(tocheck)]
-            print('ID', person_id)
-            diag('keypad.py: Code of ID: ' + str( person_id))
-            if checkcamera(person_id):
-                diag('keypad.py: Door unlocked')
+        if (tocheck != ""):
+            diag('keypad.py: Code entered:' + tocheck)
+            person_id = int(noneid)
+            if (tocheck == masterpin):
+                print("masterpin entered")
+                diag('keypad.py: Door unlocked, masterpin entered')
                 door_open(3)
-            else:
-                print('Cant see you')
-            lockout = 0
-        else:
-            person_id = 30
-            print('Pin not recognised')
-            if len(code) >= 1:
-                lockout += 1
-            if lockout == 3:
-                diag('keypad.py: Lockout started')
-                print('Lockout for 10 sec')
-                sleep(10)
-                diag('keypad.py: Lockout finnished')
                 lockout = 0
-        timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
-        db.execute('INSERT INTO log ("person_id", "datetime", "distance") VALUES(?, ?, ?)',person_id, timestamp, tocheck)
-        tocheck = ''
-        code = []
+            elif (tocheck in saved_pins_pin and saved_pins_trusted[saved_pins_pin.index(tocheck)] == 'True'):
+                person_id = saved_pins_id[saved_pins_pin.index(tocheck)]
+                print('ID', person_id)
+                diag('keypad.py: Code of ID: ' + str( person_id))
+                if checkcamera(person_id):
+                    diag('keypad.py: Door unlocked')
+                    door_open(3)
+                else:
+                    print('Cant see you')
+                lockout = 0
+            else:
+                print('Pin not recognised')
+                if len(code) >= 1:
+                    lockout += 1
+                if lockout == 3:
+                    diag('keypad.py: Lockout started')
+                    print('Lockout for 10 sec')
+                    sleep(10)
+                    diag('keypad.py: Lockout finnished')
+                    lockout = 0
+            timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+            db.execute('INSERT INTO log ("person_id", "datetime", "distance") VALUES(?, ?, ?)',person_id, timestamp, tocheck)
+            tocheck = ''
+            code = []
     #print(code)
     sleep(0.25)
